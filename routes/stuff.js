6c9d336c670644b0a37296de4ae88a69e0bd7e7e
nemanja94 <nemanja.andric.1994@outlook.com>
2019-10-13 20:18:59 +0200

    Omoguceno dodavanje slike

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,85 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const Thing = require('../models/thing');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', (req, res, next) => {
-    const thing = new Thing({
-        'title': req.body.title,
-        'description': req.body.description,
-        'imageUrl': req.body.imageUrl,
-        'price': req.body.price,
-        'userId': req.body.userId
-    });
-    thing.save().then(() => {
-        res.status(201).json({
-            'message': 'Post saved successfully!'
-        });
-    })
-        .catch((error) => {
-            res.status(400).json({
-                error
-            });
-        });
-});
+const stuffCtrl = require('../controllers/stuff');
 
-router.get('/:id', (req, res, next) => {
-    Thing.findOne({
-        '_id': req.params.id
-    }).then((thing) => {
-        res.status(200).json(thing);
-    })
-        .catch((error) => {
-            res.status(404).json({
-                error
-            });
-        });
-});
-
-router.put('/:id', (req, res, next) => {
-    const thing = new Thing({
-        '_id': req.params.id,
-        'title': req.body.title,
-        'description': req.body.description,
-        'imageUrl': req.body.imageUrl,
-        'price': req.body.price,
-        'userId': req.body.userId
-    });
-    Thing.updateOne({ '_id': req.params.id }, thing).then(() => {
-        res.status(201).json({
-            'message': 'Thing updated successfully!'
-        });
-    })
-        .catch((error) => {
-            res.status(400).json({
-                error
-            });
-        });
-});
-
-router.delete('/:id', (req, res, next) => {
-    Thing.deleteOne({ '_id': req.params.id }).then(() => {
-        res.status(200).json({
-            'message': 'Deleted!'
-        });
-    })
-        .catch((error) => {
-            res.status(400).json({
-                error
-            });
-        });
-});
-
-router.get('/' +
-  '', (req, res, next) => {
-    Thing.find().then((things) => {
-        res.status(200).json(things);
-    })
-        .catch((error) => {
-            res.status(400).json({
-                error
-            });
-        });
-});
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, multer, stuffCtrl.createThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
+router.put('/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 module.exports = router;
