b0bcc78a15d5b943dc5332b07269f611d977abf7
nemanja94 <nemanja.andric.1994@outlook.com>
2019-10-17 08:24:10 +0200

    Promenjen port

--- server.js
+++ server.js
@@ -1,47 +1,61 @@
-const http = require('http');
-const app = require('./app');
+// const http = require('http');
+// const app = require('./app');
 
-const normalizePort = (val) => {
-    const port = parseInt(val, 10);
+// const normalizePort = (val) => {
+//     const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+//     if (isNaN(port)) {
+//         return val;
+//     }
+//     if (port >= 0) {
+//         return port;
+//     }
+//     return false;
+// };
+// const port = normalizePort(process.env.PORT || '3000');
+// app.set('port', port);
 
-const errorHandler = (error) => {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const address = server.address();
-    const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
-    switch (error.code) {
-        case 'EACCES':
-            console.error(`${bind} requires elevated privileges.`);
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(`${bind} is already in use.`);
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-};
+// const errorHandler = (error) => {
+//     if (error.syscall !== 'listen') {
+//         throw error;
+//     }
+//     const address = server.address();
+//     const bind = typeof address === 'string' ? `pipe ${address}` : `port: ${port}`;
+//     switch (error.code) {
+//         case 'EACCES':
+//             console.error(`${bind} requires elevated privileges.`);
+//             process.exit(1);
+//             break;
+//         case 'EADDRINUSE':
+//             console.error(`${bind} is already in use.`);
+//             process.exit(1);
+//             break;
+//         default:
+//             throw error;
+//     }
+// };
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
-    console.log(`Listening on ${bind}`);
+// server.on('error', errorHandler);
+// server.on('listening', () => {
+//     const address = server.address();
+//     const bind = typeof address === 'string' ? `pipe ${address}` : `port ${port}`;
+//     console.log(`Listening on ${bind}`);
+// });
+
+// server.listen(port);
+
+const express = require('express');
+
+const publicweb = process.env.PUBLICWEB || '../dist/frontend';
+const app = express();
+
+app.use(express.static(publicweb));
+console.log(`serving ${publicweb}`);
+app.get('*', (req, res) => {
+    res.sendFile(`index.html`, { root: publicweb });
 });
 
-server.listen(port);
+const port = process.env.SERVER_PORT || '3000';
+app.listen(port, () => console.log(`API running on localhost:${port}`));
